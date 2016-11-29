9638e8525cd711ef6b92a7bb4028a4c4dc30a60f
Dafrok <o.o@mug.dog>
2016-11-29 18:57:56 +0800

    Prepare

---
+++ src/index.vue
@@ -0,0 +1,27 @@
+<template lang="pug">
+form(@submit.prevent="submit")
+  slot
+</template>
+
+<script>
+export default {
+  props: ['accept-charset', 'action', 'autocomplete', 'enctype', 'method', 'name', 'novaidate', 'target'],
+  methods: {
+    submit (e) {
+      const {action,  acceptCharset, autoComplete, enctype, method, name, novalidate, target} = this
+
+      const form = new FormData()
+
+      const options = {
+        headers: {
+        },
+        methods: method,
+        body: form
+      }
+      fetch(action, options)
+        .then(res => this.$emit('response'))
+        .catch(res => this.$emit('failed'))
+    }
+  }
+}
+</script>
