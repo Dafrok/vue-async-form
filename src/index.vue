64f36ec274f58ab184a35e4cdb920050cfe0ac81
Dafrok <o.o@mug.dog>
2016-11-30 19:50:03 +0800

    Serilize form

--- src/index.vue
+++ src/index.vue
@@ -7,16 +7,26 @@ form(@submit.prevent="submit")
 export default {
   props: ['accept-charset', 'action', 'autocomplete', 'enctype', 'method', 'name', 'novaidate', 'target'],
   methods: {
+    _serilize ($form) {
+      const formData = new FormData()
+      const data = {}
+      for (let i = 0; i < $form.length; i++) {
+        if (data[name]) {
+          data[name].push($form[i].value)
+        } else {
+          data[name] = [$form[i].value]
+        }
+      }
+    }
     submit (e) {
       const {action,  acceptCharset, autoComplete, enctype, method, name, novalidate, target} = this
-
-      const form = new FormData()
+      const formData = this._serilize(e.target)
 
       const options = {
         headers: {
         },
         methods: method,
-        body: form
+        body: formData
       }
       fetch(action, options)
         .then(res => this.$emit('response'))
